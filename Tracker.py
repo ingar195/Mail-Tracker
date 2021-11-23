b009c8a776adee0e503e3c3e5b5bb70e2725f852
ingar195 <ingar@megarden.no>
2021-11-23 17:15:23 +0100

    New logger

--- Tracker.py
+++ Tracker.py
@@ -63,7 +63,14 @@ def Menu():
     pass
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%d-%m-%Y:%H:%M:%S',
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("Tracker.log"),
+        logging.StreamHandler()
+    ])
 
 pb = Pushbullet(ReadFile("pushbulletapikey"))
 
