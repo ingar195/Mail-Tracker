8a096df142d06b7827c4df648e01e222c5820ff6
ingar <ingar@megarden.no>
2023-06-06 00:04:38 +0200

    web stuff

---
+++ static/script.js
@@ -0,0 +1,26 @@
+console.log("script.js loaded")
+
+//function run the update function when the page loads
+window.onload = updateSelectList
+
+// function to update the select list
+function updateSelectList() {
+    // Get the selected value
+    selected = document.getElementById("packageList").value
+    // Get the new content
+    fetch("/api/" + selected)
+        .then(response => response.json())
+        .then(data => setSelectContent("select", data))
+}
+
+
+// Populate select list with stuff
+function setSelectContent(selectID, content) {
+    list = document.getElementById(selectID)
+    list.replaceChildren()
+    content.forEach((entry) => {
+        list.appendChild(
+            new Option(entry, entry)
+        )
+    })
+}
