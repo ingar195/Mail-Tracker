bb15b9fbdf200c57b4201c3206677b916536291d
ingar <ingar@megarden.no>
2023-06-11 18:37:32 +0200

    Added separate file for alerts

---
+++ alert.py
@@ -0,0 +1,25 @@
+import logging
+import tracker
+import time
+
+if __name__ == '__main__':
+    logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%d-%m-%Y:%H:%M:%S',
+    level=logging.DEBUG,
+    handlers=[
+        logging.FileHandler("log.log"),
+        logging.StreamHandler()
+    ])
+    
+    while True:
+        tracker.update_all()
+        logging.debug("Updated all packages")
+        conf = tracker.read_config("config.json")
+        if "config" in conf:
+            interval = conf["config"].get("interval")
+            if not interval:
+                interval = 60
+            time.sleep(interval)
+            
+        
